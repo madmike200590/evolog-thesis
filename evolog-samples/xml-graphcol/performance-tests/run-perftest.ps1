65b6d047914f527d4c87be5be4d40961ca07353d
Michael Langowski <michael.langowski@gmx.at>
2025-03-01 13:14:17 +0100

    performance test results

--- evolog-samples/xml-graphcol/performance-tests/run-perftest.ps1
+++ evolog-samples/xml-graphcol/performance-tests/run-perftest.ps1
@@ -1,5 +1,6 @@
 $specPath = "./filespecs"
-$outfile = "./output/perftest.out"
+$outfile_classic = "./output/classic_perftest.out"
+$outfile_evl = "./output/evl_perftest.out"
 
 $classicAspInPath = "./infiles_classic"
 $classicAspOutPath = "./output"
@@ -9,14 +10,14 @@ Get-ChildItem -Path "output/" -Include *.* -File -Recurse | foreach { $_.Delete(
 
 $infilesAsp = Get-ChildItem -Path "$classicAspInPath"
 echo "Running Classic ASP on Alpha Tests"
-ForEach ($infile in $infile) {
-	echo "Running perftest $filespec" >> $outfile
-	Measure-Command { alpha-solver -q -n6 -i .\src\3col-classic.asp -i $infile } *>> $outfile
+ForEach ($infile in $infilesAsp) {
+	echo "Running perftest $infile" >> $outfile_classic
+	Measure-Command { alpha-solver -q -n6 -i .\src\3col-classic.asp -i "$classicAspInPath/$infile" } *>> $outfile_classic
 }
 
 $filespecs = Get-ChildItem -Path "$specPath"
 echo "Running Evolog Tests"
 ForEach ($filespec in $filespecs) {
 	echo "Running perftest $filespec" >> $outfile
-	Measure-Command { alpha-solver -q -i .\src\perftest-file-based-3col.evl -i .\src\xml_dom.mod.evl -i .\src\3col-module-with-lists.evl -i .\src\3col_to_xml.mod.evl -i "$specPath/$filespec" } *>> $outfile
+	Measure-Command { alpha-solver -q -i .\src\perftest-file-based-3col.evl -i .\src\xml_dom.mod.evl -i .\src\3col-module-with-lists.evl -i .\src\3col_to_xml.mod.evl -i "$specPath/$filespec" } *>> $outfile_evl
 }
