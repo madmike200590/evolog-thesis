f2d076ded812670f54770f4c65a5e7134016afcf
Michael Langowski <michael.langowski@gmx.at>
2025-03-01 14:20:33 +0100

    performance test results incorporated into text

--- evolog-samples/xml-graphcol/performance-tests/run-perftest.ps1
+++ evolog-samples/xml-graphcol/performance-tests/run-perftest.ps1
@@ -18,6 +18,6 @@ ForEach ($infile in $infilesAsp) {
 $filespecs = Get-ChildItem -Path "$specPath"
 echo "Running Evolog Tests"
 ForEach ($filespec in $filespecs) {
-	echo "Running perftest $filespec" >> $outfile
+	echo "Running perftest $filespec" >> $outfile_evl
 	Measure-Command { alpha-solver -q -i .\src\perftest-file-based-3col.evl -i .\src\xml_dom.mod.evl -i .\src\3col-module-with-lists.evl -i .\src\3col_to_xml.mod.evl -i "$specPath/$filespec" } *>> $outfile_evl
 }
